675e740cd7bf3b8f500b1d79e83171fffee09a09
e-pena <63423138+e-pena@users.noreply.github.com>
2020-08-26 00:12:54 -0300

    Mejor

--- db/02.CREATE_TABLE_ESTADO_PEDIDOS.sql
+++ db/02.CREATE_TABLE_ESTADO_PEDIDOS.sql
@@ -2,5 +2,3 @@ CREATE TABLE estados (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     estado VARCHAR (255) NOT NULL
 )
-
-INSERT INTO estados (`estado`) VALUES ('Nuevo'), ('Confirmado'), ('Preparando'), ('Enviando'), ('Cancelado'), ('Entregado')
