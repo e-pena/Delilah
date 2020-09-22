c0a5d365b52cc120388b10018263e54b22629eca
e-pena <63423138+e-pena@users.noreply.github.com>
2020-09-22 03:07:07 -0300

    Arreglo base de datos y funciones POST pedido

--- db/07.CREATE_TABLE_PRODUCTOPEDIDO.sql
+++ db/07.CREATE_TABLE_PRODUCTOPEDIDO.sql
@@ -2,6 +2,7 @@ CREATE TABLE productoPedido (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     pedido_id INT NOT NULL,
     producto_id INT NOT NULL,
+    cantidad INT NOT NULL,
     CONSTRAINT FK_pedidoID FOREIGN KEY (pedido_id)
     REFERENCES pedidos (id),
     CONSTRAINT FK_productoID FOREIGN KEY (producto_id)
