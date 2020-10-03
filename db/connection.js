0058b380a14d715d55331d15f9888ad42c9a2796
e-pena <63423138+e-pena@users.noreply.github.com>
2020-10-03 00:47:19 -0300

    arreglos conexión db

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sql = new Sequelize(`mysql://b3b8419f087148:6ec29535@us-cdbr-east-02.cleardb.com/heroku_34ecf3590bb8583`);
+const sql = new Sequelize(`mysql://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_DB}`);
 
 module.exports = sql;
-
-// mysql://b3b8419f087148:6ec29535@us-cdbr-east-02.cleardb.com/heroku_34ecf3590bb8583?reconnect=true
